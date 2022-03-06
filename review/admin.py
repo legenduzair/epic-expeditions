671ead706f598800517fd0d4a270527818a56707
legenduzair <uzairoomer@live.com>
2022-03-06 21:28:51 +0000

    Add django admin features to admin.py for easy accessbility of reviews

--- review/admin.py
+++ review/admin.py
@@ -5,6 +5,10 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(TravelReview)
 class TravelReviewAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('published', 'author')
+    list_display = ('title', 'author', 'content', 'published', 'ratings')
+    search_fields = ('title', 'author')
     summernote_fields = ('content')
 
 
