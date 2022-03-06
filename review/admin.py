b6568f0659c8f802b923ef1e1a14fde0c642892b
legenduzair <uzairoomer@live.com>
2022-03-06 21:12:32 +0000

    Install Django summernote library and add url path, to settings.py and admin.py

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import TravelReview
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(TravelReview)
+class TravelReviewAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
+
+
+
