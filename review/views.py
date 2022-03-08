f1aa1897a068b6be6fcfcd6fd0bfcf7e7ffc7607
legenduzair <uzairoomer@live.com>
2022-03-08 20:28:31 +0000

    Add first class based view for rendering reviews on page

--- review/views.py
+++ review/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import TravelReview
 
 # Create your views here.
+
+class TravelReviewList(ListView):
+    model = TravelReview
+    template_name = #
+    ordering = ['-published']
+    paginate_by = 8
+
+
