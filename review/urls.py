505ffff415153c8d6bf160f507aaf698d021cc1e
legenduzair <uzairoomer@live.com>
2022-03-12 16:35:48 +0000

    Add anchor tag to review_list html to route to review_detail page

--- review/urls.py
+++ review/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('review_list/', TravelReviewList.as_view(), name='review_list'),
-    path('review_detail/<int:id>/', views.review_detail, name='review_details'),
+    path('review_detail/<int:id>/', views.review_detail, name='review_detail'),
 ]
