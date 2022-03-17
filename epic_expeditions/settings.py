5bb2ff888597987ec37529f69eefac251dffa2ac
legenduzair <uzairoomer@live.com>
2022-03-17 21:11:23 +0000

    install crispy forms and add to add_review.html, and add crispy forms to settings.py

--- epic_expeditions/settings.py
+++ epic_expeditions/settings.py
@@ -46,9 +46,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'review',
     'members',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
