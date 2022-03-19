18578aaf440ecc4cc5958b5a86b3575f7d7a492c
legenduzair <uzairoomer@live.com>
2022-03-19 14:18:08 +0000

    Initiate script.js to implement script for star ratings

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+const one = document.getElementById('first')
+const two = document.getElementById('second')
+const three = document.getElementById('third')
+const four = document.getElementById('fourth')
+const five = document.getElementById('fifth')
+
+const form = document.querySelector('.form')
+
+const arr = [one, two, three, four, five]
+
