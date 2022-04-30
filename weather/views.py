a97089ee15b3a6c5f2fdfd088dfc8e3cf0b30993
legenduzair <uzairoomer@live.com>
2022-04-30 18:25:50 +0000

    Edit weather.html weather card text, edit views.py to add current time of each city and style weather card in css

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
+import urllib.parse
 import urllib.request
 import json
+from datetime import datetime
 
 # Create your views here.
 
@@ -13,23 +15,50 @@ class WeatherView(TemplateView):
 
 def search_weather(request):
     if request.method == 'POST':
-        city = request.POST['city']
+        city = urllib.parse.quote_plus(request.POST['city'])
         source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' +
                                         city + '&units=metric&appid=8cc5e423f92f5911e4369f44a1e605be').read()
         list_of_data = json.loads(source)
-
+        
         data = {
             "city": city,
+            "dt": (list_of_data['dt']),
             "temp": str(list_of_data['main']['temp']) + ' °C',
             "humidity": str(list_of_data['main']['humidity']),
-            "wind": str(list_of_data['wind']['speed']),
-            "timezone": str(list_of_data['timezone']),
-            "main": str(list_of_data['weather'][0]['main']),
+            "wind": str(list_of_data['wind']['speed']*2),
+            "pressure": str(list_of_data['main']['pressure']),
+            "timezone": (list_of_data['timezone']),
             "description": str(list_of_data['weather'][0]['description']),
             "icon": str(list_of_data['weather'][0]['icon']),
         }
-        print(data)
+
+        dt = data['dt']
+        timezone = data['timezone']
+        current_time = utctime_and_timezone(dt + timezone)
+        print(current_time)
+
+        data = {
+            "city": city,
+            "dt": (list_of_data['dt']),
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "humidity": str(list_of_data['main']['humidity']),
+            "wind": str(list_of_data['wind']['speed']*2),
+            "pressure": str(list_of_data['main']['pressure']),
+            "timezone": (list_of_data['timezone']),
+            "description": str(list_of_data['weather'][0]['description']),
+            "icon": str(list_of_data['weather'][0]['icon']),
+            "current_time": current_time,
+        }
+
     else:
         data = {}
-
+    
     return render(request, "weather.html", data)
+
+
+def utctime_and_timezone(utc_with_tz):
+    local_time = datetime.utcfromtimestamp(utc_with_tz)
+    return local_time.time()
+
+
+
