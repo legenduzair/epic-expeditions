6387db78baaf3e2f68b3cd7a2ae7b7471e488fbd
legenduzair <uzairoomer@live.com>
2022-05-05 21:26:07 +0000

    Write function based views to load error templates

--- errors/views.py
+++ errors/views.py
@@ -2,3 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+def page_not_found_view(request, exception):
+
+    return render(request, 'errors/404.html', {})
+
+
+def my_custom_error_view(request, exception=None):
+
+    return render(request, 'errors/500.html', {})
+
+
+def my_custom_permission_denied_view(request, exception=None):
+
+    return render(request, 'errors/403.html', {})
+
+
+def my_custom_bad_request_view(request, exception=None):
+
+    return render(request, 'errors/400.html', {})
