7da3eb5e8cd9b84e1e7e327e1f6c16cb18e73f64
legenduzair <uzairoomer@live.com>
2022-05-08 14:49:45 +0000

    Add missing semicolons after pasting js code in JSHint

--- static/js/script.js
+++ static/js/script.js
@@ -1,51 +1,51 @@
-const one = document.getElementById('first')
-const two = document.getElementById('second')
-const three = document.getElementById('third')
-const four = document.getElementById('fourth')
-const five = document.getElementById('fifth')
+const one = document.getElementById('first');
+const two = document.getElementById('second');
+const three = document.getElementById('third');
+const four = document.getElementById('fourth');
+const five = document.getElementById('fifth');
 
-const form = document.querySelector('.add-review')
+const form = document.querySelector('.add-review');
 
 const handleSelect = (selection) => {
     switch(selection) {
         case 'first': {
-            one.classList.add('checked')
-            two.classList.remove('checked')
-            three.classList.remove('checked')
-            four.classList.remove('checked')
-            five.classList.remove('checked')
+            one.classList.add('checked');
+            two.classList.remove('checked');
+            three.classList.remove('checked');
+            four.classList.remove('checked');
+            five.classList.remove('checked');
             return;
         }
         case 'second': {
-            one.classList.add('checked')
-            two.classList.add('checked')
-            three.classList.remove('checked')
-            four.classList.remove('checked')
-            five.classList.remove('checked')
+            one.classList.add('checked');
+            two.classList.add('checked');
+            three.classList.remove('checked');
+            four.classList.remove('checked');
+            five.classList.remove('checked');
             return;
         }
         case 'third': {
-            one.classList.add('checked')
-            two.classList.add('checked')
-            three.classList.add('checked')
-            four.classList.remove('checked')
-            five.classList.remove('checked')
+            one.classList.add('checked');
+            two.classList.add('checked');
+            three.classList.add('checked');
+            four.classList.remove('checked');
+            five.classList.remove('checked');
             return;
         }
         case 'fourth': {
-            one.classList.add('checked')
-            two.classList.add('checked')
-            three.classList.add('checked')
-            four.classList.add('checked')
-            five.classList.remove('checked')
+            one.classList.add('checked');
+            two.classList.add('checked');
+            three.classList.add('checked');
+            four.classList.add('checked');
+            five.classList.remove('checked');
             return;
         }
         case 'fifth': {
-            one.classList.add('checked')
-            two.classList.add('checked')
-            three.classList.add('checked')
-            four.classList.add('checked')
-            five.classList.add('checked')
+            one.classList.add('checked');
+            two.classList.add('checked');
+            three.classList.add('checked');
+            four.classList.add('checked');
+            five.classList.add('checked');
             return;
         }
     }
