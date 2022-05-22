31fd692a471f5ab8e53ddcca05f66460ee04ed31
legenduzair <uzairoomer@live.com>
2022-05-22 20:26:12 +0000

    Edit test in test_views.py and edit error views in views.py in errors app

--- errors/test_views.py
+++ errors/test_views.py
@@ -5,5 +5,5 @@ class TestErrors(TestCase):
 
     def test_page_not_found(self):
 
-        response = self.client.get('/review_list/0/')
+        response = self.client.get('/review_detail/0/')
         self.assertEqual(response.status_code, 404)
--- errors/views.py
+++ errors/views.py
@@ -6,19 +6,19 @@ from django.shortcuts import render
 
 def page_not_found_view(request, exception):
     """ Function to render 404 error page """
-    return render(request, 'errors/404.html', {})
+    return render(request, 'errors/404.html', status=404)
 
 
-def my_custom_error_view(request, exception=None):
+def my_custom_error_view(request, exception):
     """ Function to render 500 error page """
-    return render(request, 'errors/500.html', {})
+    return render(request, 'errors/500.html', status=500)
 
 
-def my_custom_permission_denied_view(request, exception=None):
+def my_custom_permission_denied_view(request, exception):
     """ Function to render 403 error page """
-    return render(request, 'errors/403.html', {})
+    return render(request, 'errors/403.html', status=403)
 
 
-def my_custom_bad_request_view(request, exception=None):
+def my_custom_bad_request_view(request, exception):
     """ Function to render 400 error page """
-    return render(request, 'errors/400.html', {})
+    return render(request, 'errors/400.html', status=400)
