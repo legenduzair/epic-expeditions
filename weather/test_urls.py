ae7e3b796d9beff749920d9c915280252f49e443
legenduzair <uzairoomer@live.com>
2022-05-25 12:23:00 +0000

    Add docstring to weather url test function

--- weather/test_urls.py
+++ weather/test_urls.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 
 
 class UrlWeatherTest(TestCase):
-
+    """URL Loading Test"""
     def test_weather_url(self):
-
+        """Test to observe if weather page URL is loading correctly"""
         response = self.client.get('')
         self.assertEqual(response.status_code, 200)
